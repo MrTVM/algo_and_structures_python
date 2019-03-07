b3a9a58c428a345b931511890e4c63e869be1d45
mrTVM <tv1adimir2011@gmail.com>
2019-03-07 23:50:08 +0300

    Выполнил задание 6 урока 1. Определение буквы латинского алфавита по номеру.

--- Lesson_1/6.py
+++ Lesson_1/6.py
@@ -1,1 +1,11 @@
 # 6.	Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+
+letter_number = int(input('Введите номер буквы в алфавите (от 1 до 26):'))
+
+number_first_letter_in_alphabet = ord('a')
+pos_letter_number = number_first_letter_in_alphabet + letter_number - 1
+letter = chr(pos_letter_number)
+
+print('{} буква в алфавите - буква {}'.format(letter_number, letter))
+

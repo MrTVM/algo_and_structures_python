24a8b40f023e0c5e5ba3a17713d775364cd562ae
mrTVM <tv1adimir2011@gmail.com>
2019-03-07 23:18:32 +0300

    Выполнил задание 4 урока 1. Генерация случайных чисел.

--- Lesson_1/4.py
+++ Lesson_1/4.py
@@ -8,3 +8,24 @@
 то вводятся эти символы. Программа должна вывести на экран любой
 символ алфавита от 'a' до 'f' включительно.
 """
+import random
+
+min_number_range = int(input('Введите число (нижнюю границу диапазона):'))
+max_number_range = int(input('Введите число (верхнюю границу диапазона):'))
+
+number_random_int = random.randint(min_number_range, max_number_range + 1)
+print('Случайное целое число: {}'.format(number_random_int))
+
+
+min_number_range = int(input('Введите число (нижнюю границу диапазона):'))
+max_number_range = int(input('Введите число (верхнюю границу диапазона):'))
+
+number_random_uni = random.uniform(min_number_range, max_number_range + 1)
+print('Случайное вещественное число: {}'.format(number_random_uni))
+
+first_letter = str(input('Введите с какой буквы начать:')).lower()
+last_letter = str(input('Введите какой буквой закончить:')).lower()
+
+number_letter_int = random.randint(ord(first_letter), ord(last_letter) + 1)
+print('Случайный символ: {}'.format(chr(number_letter_int)))
+

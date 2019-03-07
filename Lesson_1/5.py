44e5b6fa5ad40402ce349d99ee3cb8def60be4d6
mrTVM <tv1adimir2011@gmail.com>
2019-03-07 23:39:33 +0300

    Выполнил задание 5 урока 1. Определить позицию букв и расстояние между ними.

--- Lesson_1/5.py
+++ Lesson_1/5.py
@@ -1,2 +1,19 @@
 #5.	Пользователь вводит две буквы. Определить, на каких местах
 # алфавита они стоят, и сколько между ними находится букв.
+
+first_letter = input('Введите первую букву:').lower()
+second_letter = input('Введите вторую букву').lower()
+
+start_number_in_alphabet = ord('a')
+pos_first_letter = ord(first_letter) - start_number_in_alphabet + 1
+pos_second_letter = ord(second_letter) - start_number_in_alphabet + 1
+distance = pos_second_letter - pos_first_letter
+
+print('Позиция буквы {} в алфавите: {}'.format(first_letter, pos_first_letter))
+print('Позиция буквы {} в алфавите: {}'.format(second_letter, pos_second_letter))
+if distance > 0:
+    print('Между буквами {} симв.'.format(distance))
+else:
+    print('Между буквами {} симв.'.format(distance*-1))
+
+

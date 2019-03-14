b328b5252e32d344065989187156c20c3585e442
mrTVM <tv1adimir2011@gmail.com>
2019-03-14 23:22:00 +0300

    Выполнил задание 2 урока 3. Список индексов четных чисел.

--- Lesson_3/2.py
+++ Lesson_3/2.py
@@ -5,3 +5,23 @@
 (или 0, 3, 4, 5 - если индексация начинается с нуля),
 т.к. именно в этих позициях первого массива стоят четные числа.
 """
+
+from random import random, randint
+
+N = 10
+numbers = [0] * N
+
+for i in range(N):
+    val = randint(0, 100)
+    numbers[i] = val
+print(numbers)
+
+even_num = []
+count = 1
+for number in numbers:
+    if number % 2 == 0:
+        even_num.append(count)
+    count += 1
+print(even_num)
+
+

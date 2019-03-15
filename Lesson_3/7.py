bdada3f4bc7d41cbacd403981a3f54c9acca8844
mrTVM <tv1adimir2011@gmail.com>
2019-03-15 23:07:06 +0300

    Выполнил задание 7 урока 3. В массиве определить два наименьших числа.

--- Lesson_3/7.py
+++ Lesson_3/7.py
@@ -3,3 +3,31 @@
 Они могут быть как равны между собой (оба являться минимальными),
  так и различаться.
 """
+
+
+from random import random, randint
+
+N = 10
+numbers = [0] * N
+
+for i in range(N):
+    val = randint(1, 10)
+    numbers[i] = val
+print(numbers)
+
+
+first_min_num = numbers[0]
+second_min_num = numbers[0]
+
+for number in numbers:
+    for num in numbers:
+        if number < first_min_num:
+            first_min_num = number
+        if numbers.count(first_min_num) >= 2:
+            second_min_num = first_min_num
+        elif first_min_num < number < second_min_num:
+            second_min_num = number
+print(first_min_num, second_min_num)
+
+
+

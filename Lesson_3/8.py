7bc967e32b07d2143da0ffb9d107d55fa34068b1
mrTVM <tv1adimir2011@gmail.com>
2019-03-15 23:48:27 +0300

    Выполнил задание 8 урока 3. Заполнение матрицы 5x4.

--- Lesson_3/8.py
+++ Lesson_3/8.py
@@ -4,3 +4,22 @@
 записывать ее в последнюю ячейку строки.
 В конце следует вывести полученную матрицу.
 """
+
+N = 5
+M = 4
+matrix_arr = []
+
+
+for i in range(M):
+    user_arr = []
+    user_sum = 0
+    for j in range(N - 1):
+        user_num = int(input('Введите число: '))
+        user_arr.append(user_num)
+        user_sum += user_num
+        print(user_num, end=' ')
+    user_arr.append(user_sum)
+    print(user_sum)
+    matrix_arr.append(user_arr)
+print(matrix_arr)
+

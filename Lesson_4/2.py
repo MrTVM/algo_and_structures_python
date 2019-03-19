08c9b92ec4a3fd8551dcaaec35d2c6c02d61137c
mrTVM <tv1adimir2011@gmail.com>
2019-03-19 23:44:08 +0300

    Выполнил задание 2 урока 4. Найти i-й элемент простых числел.

--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,38 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+
+# составляем список простых чисел из последовательности длинной n
+n = int(input("n="))
+lst = []
+for i in range(2, n+1):
+    for j in range(2, i):
+        if i % j == 0:
+            # если делитель найден, число не простое.
+            break
+    else:
+        lst.append(i)
+print(lst)
+
+# ищем i-й элемент в списке простых чисел
+user_num = int(input('Введите номер искомого элемента:'))
+print(lst[user_num - 1])
+
+
+# Решето Эратосфена
+a = list(range(n + 1))
+a[1] = 0
+lst = []
+
+i = 2
+while i <= n:
+    if a[i] != 0:
+        lst.append(a[i])
+        for j in range(i, n+1, i):
+            a[j] = 0
+    i += 1
+print(lst)
+print(lst[user_num - 1])
+
+
+

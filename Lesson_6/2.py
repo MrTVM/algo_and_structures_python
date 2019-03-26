8ebdbf89e053a7c83c963fb47fe6b178194794a2
mrTVM <tv1adimir2011@gmail.com>
2019-03-27 00:24:46 +0300

    Выполнил задание 2 урока 6. Сравнение использования памяти атрибутами класса.

--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,30 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+
+from pympler import asizeof
+
+class Pearson:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.damage = 50
+        self.armor = 1.2
+
+class Pearson_slots:
+    __slots__ = ['name', 'health', 'damage', 'armor']
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.damage = 50
+        self.armor = 1.2
+
+
+
+mr = Pearson('имя')
+mrs = Pearson_slots('имя')
+
+print(mr.__dict__)
+
+print(asizeof.asizeof(mr))
+print(asizeof.asizeof(mrs))

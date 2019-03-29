ef3e75cee954134b40b8b073bf6e94517744c1b0
mrTVM <tv1adimir2011@gmail.com>
2019-03-29 23:55:14 +0300

    Выполнил задание 1 урока 7. Добавли останов цикла, если ни одной замены не было за проход.

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,29 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+
+import random
+
+def buuble_sort(arr, order=False):
+    n = 1
+    while n < len(arr):
+        end_sorted = 0
+        for i in range(len(arr) - n):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                end_sorted += 1
+        if end_sorted == 0:
+            break
+        n += 1
+    if order:
+        arr = list(reversed(arr))
+    print(n)
+    return arr
+
+
+my_arr = [random.randint(-100, 100) for _ in range(10)]
+print(my_arr)
+print(buuble_sort(my_arr))
+
+
+

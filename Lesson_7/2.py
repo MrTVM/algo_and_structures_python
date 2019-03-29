bc2a583677d31a96fa1a882103fe8b13523cb1f8
mrTVM <tv1adimir2011@gmail.com>
2019-03-30 00:04:59 +0300

    Выполнил задание 2 урока 7. Сортировка слиянием.

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,46 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+
+import random
+
+def merge_sort(arr, order=False):
+    if len(arr) > 1:
+        center = len(arr) // 2
+        left = arr[:center]
+        right = arr[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+        if order:
+            arr = list(reversed(arr))
+
+        return arr
+
+
+my_arr = [random.randint(0, 50) for _ in range(10)]
+print(my_arr)
+print(merge_sort(my_arr))
+

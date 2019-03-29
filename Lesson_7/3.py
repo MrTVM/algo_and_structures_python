f3daeeb989daf89526848138e416a95417a8f356
mrTVM <tv1adimir2011@gmail.com>
2019-03-30 00:49:04 +0300

    Выполнил задание 3 урока 7. Поиск медианы не сортированного списка.

--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,27 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+
+import random
+
+def search_med_value(arr):
+    for i in range(len(arr)):
+        left_count = 0
+        for j in range(len(arr)):
+            if arr[i] >= arr[j]:
+                left_count += 1
+        if left_count == len(arr) // 2 + 1:
+            break
+    return arr[i]
+
+
+m = 5
+my_arr = [random.randint(1, 100) for _ in range(2 * m + 1)]
+print(my_arr)
+print('Медиана: ', search_med_value(my_arr))
+print('Проверка медиана = ', sorted(my_arr)[m])
+
+
+
+
+
